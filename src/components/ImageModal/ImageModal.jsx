5b07c6b5cfaf41ba1e632639610c6fddeef58201
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-02-03 00:22:44 +0200

    fix image info styles

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -2,6 +2,11 @@
 import css from './ImageModal.module.css';
 import Modal from 'react-modal';
 import { IoMdClose } from 'react-icons/io';
+import {
+  PiHeartDuotone,
+  PiUserCircleDuotone,
+  PiGlobeDuotone,
+} from 'react-icons/pi';
 // import { Slide } from 'react-slideshow-image';
 // import 'react-slideshow-image/dist/styles.css';
 
@@ -14,14 +19,19 @@ const customStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     zIndex: 10000,
-    backgroundColor: '#888888',
+    backgroundColor: 'rgba(0, 0, 0, .6)',
+    // filter: 'blur(.2)',
     padding: 0,
+    border: 0,
+    borderRadius: 10,
+    boxShadow: '0 0 2em #000000',
+
     // color: 'lightsteelblue',
   },
   overlay: {
     backgroundColor: 'rgba(0,0,0, .6)',
     // filter: 'blur(5px)',
-    zIndex: 0,
+    // zIndex: 0,
     // filter: 'blur(5px)',
   },
 };
@@ -73,14 +83,28 @@ const ImageModal = ({ isOpen, onCloseModal, currentImageItem }) => {
               style={{ border: `2px solid ${color}` }}
             />
           </div>
-          <figcaption className={css.modalCaption}>
+          <figcaption
+            className={css.modalCaption}
+            // style={{ border: `2px solid ${color}`, borderTop: 0 }}
+          >
             <h4 className={css.modalTitle}>
               {description?.replace(/^\w/, m => m.toUpperCase())}
             </h4>
             <div className={css.modalImageInfo}>
-              <div className={css.modalImageLikes}>{likes}</div>
-              <div className={css.modalAuthorName}>{name}</div>
-              <div className={css.modalAuthorLocation}>{location}</div>
+              <div className={css.modalImageLikes}>
+                <PiHeartDuotone />
+                <span>{likes}</span>
+              </div>
+              <div className={css.modalAuthorName}>
+                <PiUserCircleDuotone />
+                <span>{name}</span>
+              </div>
+              {!!location && (
+                <div className={css.modalAuthorLocation}>
+                  <PiGlobeDuotone />
+                  <span>{location}</span>
+                </div>
+              )}
             </div>
           </figcaption>
         </figure>
